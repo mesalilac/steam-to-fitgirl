a714b81bf937e2a6408bc196c180c6ca4d9f2468
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-08-08 16:12:02 +0300

    chore: fix firefox package

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,11 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-solid'],
+  manifest: {
+      browser_specific_settings: {
+          gecko: {
+              id: "@mesalilac.steam-to-fitgirl"
+          }
+      }
+  }
 });
